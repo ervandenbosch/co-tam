1692e2f8b45551de58c6ce940a4f0d935d8f3a0c
ervandenbosch <ezravdb@gmail.com>
2022-03-11 14:50:13 +0100

    upd

--- components/utils.js
+++ components/utils.js
@@ -1,5 +1,5 @@
 // prettier-ignore
-export const cdnImage = (name, folder='/public/images') => {
+export const cdnImage = (name, folder='public/images') => {
   if (process.env.NODE_ENV === "production") {
       return "https://cdn.statically.io/gh/ervandenbosch/co-tam/main/"+folder+"/"+name;
   } else {
