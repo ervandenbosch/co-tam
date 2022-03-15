3ef758c6342c6bfb936c2648497e854e6f4cf28d
ervandenbosch <ezravdb@gmail.com>
2022-03-15 21:33:23 +0100

    upd

--- data/reviews.js
+++ data/reviews.js
@@ -1,8 +1,50 @@
 export const reviews = [
   {
     user: {
+      name: "John Doe",
+      link: "https://www.google.com/maps/contrib/104722377193363723526?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegQIARBB",
+      thumbnail:
+        "https://lh3.googleusercontent.com/a/AATXAJzzApIzPoS6BB4B1CI8HP-rmFw-cqxI15NykDNU=s40-c-c0x00000000-cc-rp-mo-ba3-br100",
+      local_guide: true,
+      reviews: 27,
+    },
+    rating: 4,
+    date: "19 hours ago",
+    snippet:
+      "Prima eten. Nederlandse versies van de Vietnamese keuken. Was er langs gegaan vanwege de hoge rating. Niet geheel authentiek, vind de rating dan ook wat aan de hoge kant. Drank kaart is prijzig voor wat er geboden wordt. Doet duidelijk goede zaken via bezorg diensten.",
+  },
+  {
+    user: {
+      name: "Chloe Hlmn",
+      link: "https://www.google.com/maps/contrib/102434201457192766017?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegQIARBN",
+      thumbnail:
+        "https://lh3.googleusercontent.com/a/AATXAJyGo197IWbdZtEizgLLT9xde4sDELrSUMgKlbirkQ=s40-c-c0x00000000-cc-rp-mo-br100",
+      reviews: 5,
+    },
+    rating: 5,
+    date: "2 days ago",
+    snippet:
+      "Super tasty food and super friendly staff! Large portions and not too expensive. The food is authentic, this is the best Vietnamese restaurant in the Netherlands",
+  },
+  {
+    user: {
+      name: "Spirit of the Amazon",
+      link: "https://www.google.com/maps/contrib/101059167181529348858?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegQIARBZ",
+      thumbnail:
+        "https://lh3.googleusercontent.com/a-/AOh14Gg1pLqmGGC-lmrVsTxi54andHl8HnhNeKRUrA9nER8=s40-c-c0x00000000-cc-rp-mo-ba4-br100",
+      local_guide: true,
+      reviews: 129,
+      photos: 142,
+    },
+    rating: 5,
+    date: "3 days ago",
+    snippet:
+      "When you have a talented native chef in the kitchen, you're guaranteed a flavoursome experience. The beef pho was delicious and on point and would pass muster in Hanoi or Ho Chi Min City. Thank you.",
+  },
+  {
+    user: {
       name: "Beno Baars",
-      link: "https://www.google.com/maps/contrib/117585232823509446739?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegQIARBB",
+      link: "https://www.google.com/maps/contrib/117585232823509446739?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegQIARBl",
       thumbnail:
         "https://lh3.googleusercontent.com/a-/AOh14GiH58bXBfLO_h8RIIwP-2XwvjSgCge0DVfiYRo9iA=s40-c-c0x00000000-cc-rp-mo-ba4-br100",
       local_guide: true,
@@ -10,7 +52,7 @@ export const reviews = [
       photos: 45,
     },
     rating: 4,
-    date: "2 days ago",
+    date: "6 days ago",
     snippet: "Lekker eten bij deze Vietnamees.",
     images: [
       "https://lh5.googleusercontent.com/p/AF1QipNCn5xO6ZXxFZCfo85cxKCfcty3yJkcH8ymlhmy=w100-h100-p-n-k-no",
@@ -19,14 +61,14 @@ export const reviews = [
   {
     user: {
       name: "Marianna M",
-      link: "https://www.google.com/maps/contrib/101770104303571060178?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegQIARBP",
+      link: "https://www.google.com/maps/contrib/101770104303571060178?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegQIARBz",
       thumbnail:
         "https://lh3.googleusercontent.com/a-/AOh14GhgGHlkPM7-MruQVICnbEdi1_HY0-aTvOOKUV99Xyg=s40-c-c0x00000000-cc-rp-mo-br100",
-      reviews: 7,
+      reviews: 5,
       photos: 12,
     },
     rating: 5,
-    date: "3 days ago",
+    date: "a week ago",
     snippet:
       "Visited last night, having tried their takeaway in the past. I have travelled to Vietnam and really love the cuisine. It's nice to hear that the owners here originate in Vietnam, and seem very sweet. Great service and a nice, relaxed atmosphere. For Haarlem, I really think you cannot find better. Co Tam truly exceeded my expectations, and the meal was absolutely delicious! I highly recommend. We'll be back again soon :)",
     images: [
@@ -36,26 +78,26 @@ export const reviews = [
       "https://lh5.googleusercontent.com/p/AF1QipOSu5MJj5yfvL6Pjik1vjt_ktqqTf4MjhIDcC5d=w100-h100-p-n-k-no",
     ],
     response: {
-      date: "6 days ago",
+      date: "a week ago",
       snippet: "Thank you so much!",
     },
   },
   {
     user: {
       name: "Cesar Hennevelt",
-      link: "https://www.google.com/maps/contrib/110844555245120768559?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegQIARBh",
+      link: "https://www.google.com/maps/contrib/110844555245120768559?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegUIARCFAQ",
       thumbnail:
         "https://lh3.googleusercontent.com/a/AATXAJySgk--seJa-csVQTLT8MbYdIT3gdnhWxcs5eoQ=s40-c-c0x00000000-cc-rp-mo-br100",
       reviews: 2,
     },
     rating: 5,
-    date: "4 days ago",
+    date: "a week ago",
     snippet: "Excellent service, food is incredible!",
   },
   {
     user: {
       name: "Mahi G",
-      link: "https://www.google.com/maps/contrib/107208536186777951838?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegQIARBt",
+      link: "https://www.google.com/maps/contrib/107208536186777951838?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegUIARCRAQ",
       thumbnail:
         "https://lh3.googleusercontent.com/a/AATXAJylWQ85qVBHHvDWuk-cboFDX2N2Xv2rbkBTR_vK=s40-c-c0x00000000-cc-rp-mo-br100",
       reviews: 8,
@@ -72,12 +114,12 @@ export const reviews = [
   {
     user: {
       name: "Alessandro Mautone",
-      link: "https://www.google.com/maps/contrib/100729854589851580628?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegQIARB7",
+      link: "https://www.google.com/maps/contrib/100729854589851580628?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegUIARCfAQ",
       thumbnail:
         "https://lh3.googleusercontent.com/a-/AOh14GiTPLKMB1oM5uZw7BLEM_HUI-RDYZOtCmzIjN_kt0g=s40-c-c0x00000000-cc-rp-mo-ba5-br100",
       local_guide: true,
-      reviews: 225,
-      photos: 99,
+      reviews: 226,
+      photos: 100,
     },
     rating: 4,
     date: "a week ago",
@@ -87,64 +129,26 @@ export const reviews = [
   {
     user: {
       name: "Rafael Dijkstra",
-      link: "https://www.google.com/maps/contrib/112718362621289533202?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegUIARCHAQ",
+      link: "https://www.google.com/maps/contrib/112718362621289533202?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegUIARCrAQ",
       thumbnail:
         "https://lh3.googleusercontent.com/a/AATXAJxXxMTLY4Bbjg3M124E37amY1cU07gWm5nrc3APPw=s40-c-c0x00000000-cc-rp-mo-br100",
       reviews: 3,
     },
     rating: 4,
-    date: "2 weeks ago",
+    date: "3 weeks ago",
     snippet: "",
   },
   {
     user: {
       name: "Eline Hendrickx",
-      link: "https://www.google.com/maps/contrib/104778740427695937890?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegUIARCQAQ",
+      link: "https://www.google.com/maps/contrib/104778740427695937890?hl=en-US&sa=X&ved=2ahUKEwibxKn1-sj2AhVHl3IEHbQaBCIQvvQBegUIARC0AQ",
       thumbnail:
         "https://lh3.googleusercontent.com/a/AATXAJwk9eTp_pHQ-89HrLUzKcIIH816nzqTlkTW9Dcy=s40-c-c0x00000000-cc-rp-mo-br100",
-      reviews: 3,
+      reviews: 4,
     },
     rating: 5,
-    date: "2 weeks ago",
+    date: "3 weeks ago",
     snippet:
       "(Translated by Google) Fantastic food! Delicious authentic flavors. go there.(Original)Fantastisch eten! Heerlijke authentieke smaken. Go there.",
-  },
-  {
-    user: {
-      name: "jkz",
-      link: "https://www.google.com/maps/contrib/104215268603157554397?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegUIARCdAQ",
-      thumbnail:
-        "https://lh3.googleusercontent.com/a-/AOh14GjYrIjTzdbAXE0aF5fkzVy6shsBVGmsTMc_TXSSUw=s40-c-c0x00000000-cc-rp-mo-br100",
-    },
-    rating: 5,
-    date: "a month ago",
-    snippet: "",
-  },
-  {
-    user: {
-      name: "Oana Hristea",
-      link: "https://www.google.com/maps/contrib/106342059447111591703?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegUIARCkAQ",
-      thumbnail:
-        "https://lh3.googleusercontent.com/a-/AOh14GiD7jZP3MmwqggHMtiQnodqqPvfxtXQUMEobdXbGg=s40-c-c0x00000000-cc-rp-mo-br100",
-      reviews: 7,
-    },
-    rating: 5,
-    date: "a month ago",
-    snippet:
-      "Food = super tasty, service = lovely! Always a happy experience :)",
-  },
-  {
-    user: {
-      name: "Monica Simeoni",
-      link: "https://www.google.com/maps/contrib/114379619831543146574?hl=en-US&sa=X&ved=2ahUKEwjBq6LG4L32AhWEoHIEHQwVAiUQvvQBegUIARCvAQ",
-      thumbnail:
-        "https://lh3.googleusercontent.com/a-/AOh14GilYb_nzSaDfDvISXevnYhprOmn-HVRuW6G44OM-g=s40-c-c0x00000000-cc-rp-mo-ba3-br100",
-      local_guide: true,
-      reviews: 20,
-      photos: 50,
-    },
-    rating: 5,
-    date: "a month ago",
-    snippet: "",
   },
 ];
