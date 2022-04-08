1ccd67ce9486d15068481408eb0ee6151d209c25
ervandenbosch <ezravdb@gmail.com>
2022-04-08 09:41:47 +0200

    upd

--- pages/nl/index.js
+++ pages/nl/index.js
@@ -22,6 +22,7 @@ export default function HomeNL() {
           n.parentNode.insertBefore(l, n);
         })(window, document, "script", "https://cdn.guestplan.com/widget.js");
         _gstpln.accessKey = "a61e9fd2920eb894b1f8ca80c3b0984b5e6b6059";
+        _gstpln.open = false;
         _gstpln.useHtmlLanguage = true;`);
 
   const url =
