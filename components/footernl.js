e237a0935f7dc2bda0dbb513e72baf1ff703cd26
ervandenbosch <ezravdb@gmail.com>
2023-08-16 11:51:02 +0200

    upd

--- components/footernl.js
+++ components/footernl.js
@@ -11,7 +11,7 @@ export function FooterNL() {
             width="15px"
             alt="copyright co tam"
           />
-          2022 Cô Tâm.
+          2023 Cô Tâm.
         </div>
         <div className="ml-2">Alle rechten voorbehouden.</div>
       </div>
