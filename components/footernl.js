ec08774d5f32709ad118831ed41c79b79d6b58e5
ervandenbosch <ezravdb@gmail.com>
2025-12-26 11:21:01 +0900

    Update footernl.js

--- components/footernl.js
+++ components/footernl.js
@@ -11,7 +11,7 @@ export function FooterNL() {
             width="15px"
             alt="copyright co tam"
           />
-          2024 Cô Tâm.
+          2026 Cô Tâm.
         </div>
         <div className="ml-2">Alle rechten voorbehouden.</div>
       </div>
